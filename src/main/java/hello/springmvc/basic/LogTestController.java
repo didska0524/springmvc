5e25505640f7e9af76d2db0f7b2f399c97b83f14
DESKTOP-KT09742\seoil <didska0524@gmail.com>
2023-05-31 14:20:39 +0900

    2023 05 31 commit - 주석추가

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j // 롬복이 제공하는 어노테이션, 밑줄 주석처리된 코드를 자동으로 넣어준다
-@RestController
+@RestController // @Controller 와 @ResponseBody 어노테이션 두개가 합쳐진 어노테이션이라고 이해하면 된다.
 public class LogTestController {
 
     //private final Logger log = LoggerFactory.getLogger(getClass());
